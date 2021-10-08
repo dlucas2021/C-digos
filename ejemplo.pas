d13cf99310811d056382c64957dfc98b5c525d00
Diego <you@example.com>
2021-10-08 12:40:29 +0200

    nuevo

---
+++ ejemplo.pas
@@ -0,0 +1,20 @@
+{$mode objfpc}{$H-}{$R+}{$T+}{$Q+}{$V+}{$D+}{$X-}{$warnings on}
+program ejemplo ;
+
+const
+
+  X: integer = 5;
+  Y: integer = 289;
+  
+begin
+
+  write ( 'X vale:');
+  writeln ( X );
+  
+  write ( 'Y vale:');
+  writeln ( Y );
+  
+  write ( 'la suma vale:');
+  writeln ( X + Y ); 
+  
+end.
